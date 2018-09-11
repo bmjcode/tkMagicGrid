d1e0ee4344b0b576ec6eb958fa4e685da335b54e
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2018-09-11 11:04:55 -0400

    Test Boolean values and multi-column cells in numeric_values.py

--- test/numeric_values.py
+++ test/numeric_values.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-"""Test program for displaying numeric values in cells."""
+"""Test program for displaying numeric values in cells.
+
+If the "-e" argument is present, the values will be displayed in editable
+Entry widgets. Otherwise, the values will be displayed read-only.
+"""
 
 import os
 import sys
@@ -53,6 +57,22 @@ def test():
                 mg.add_cell(value)
 
         mg.end_row()
+
+    # Test Boolean values too, while we're at it
+    mg.add_cell("Boolean values:", columnspan=3)
+    mg.add_cell(True)
+    mg.add_cell(False)
+
+    # Pad out the rest of the row
+    for column in range(5, num_columns):
+        mg.add_cell()
+    mg.end_row()
+
+    # Highlight the last row
+    mg.configure_row(num_rows + 1, bg="yellow")
+
+    # Highlight the last column, too
+    mg.configure_column(num_columns - 1, bg="orange")
 
     # Make all columns equal width
     for column in range(num_columns):
